547fe3655f0969e60feebb329af1ed664b6322d8
Steven Sakayan <ssakayan@live.com>
2025-10-20 13:18:09 +0100

    Added new skills

--- src/SkillsSection.vue
+++ src/SkillsSection.vue
@@ -6,26 +6,37 @@ import Window from './WindowContainer.vue';
 <template>
 	<Window title="Development Skills.exe">
 		<div class="px-10 py-10 max-sm:px-4">
+      <div class="divider">Core Languages</div>
+      <div class="flex flex-wrap justify-center gap-5">
+        <Skill name="Java" image="./images/java-original.svg"/>
+        <Skill name="C#" image="./images/csharp-plain.svg"/>
+        <Skill name="Python" image="./images/python-original.svg"/>
+        <Skill name="HTML" image="./images/html5-original.svg"/>
+        <Skill name="CSS" image="./images/css3-original.svg"/>
+        <Skill name="JavaScript" image="./images/javascript-original.svg"/>
+        <Skill name="SQL" image="./images/sql.svg"/>
+		  </div>
+
+      <div class="divider py-10">Libraries & Frameworks</div>
+
 			<div class="flex flex-wrap justify-center gap-5">
-			<Skill name="Java" image="./images/java-original.svg"/>
-			<Skill name="C#" image="./images/csharp-plain.svg"/>
-			<Skill name="Python" image="./images/python-original.svg"/>
-			<Skill name="HTML" image="./images/html5-original.svg"/>
-			<Skill name="CSS" image="./images/css3-original.svg"/>
-			<Skill name="JavaScript" image="./images/javascript-original.svg"/>
-			<Skill name="Vue" image="./images/vuejs-original.svg"/>
-			<Skill name="Vuetify" image="./images/vuetify-original.svg"/>
-			<Skill name="Quasar" image="./images/quasar-original.svg"/>
-			<Skill name="SQL" image="./images/sql.svg"/>
-		</div>
-		<div class="divider py-10">Other Skills</div>
-			<div class="flex flex-wrap justify-center gap-5">
-			<Skill name="Gamemaker" image="./images/gamemaker.svg"/>
-			<Skill name="Unity" image="./images/unity-plain.svg"/>
-			<Skill name="Godot" image="./images/godot-original.svg"/>
-			<Skill name="Blender" image="./images/blender-original.svg"/>
-			<Skill name="Substance Painter" image="./images/substance-painter.svg"/>
-			</div>
-		</div>
+        <Skill name="Vue" image="./images/vuejs-original.svg"/>
+        <Skill name="Vuetify" image="./images/vuetify-original.svg"/>
+        <Skill name="Quasar" image="./images/quasar-original.svg"/>
+        <Skill name="FastAPI" image="./images/fastapi-original.svg"/>
+        <Skill name="Express.js" image="./images/express-original.svg"/>
+        <Skill name="SQL" image="./images/sql.svg"/>
+		  </div>
+
+		  <div class="divider py-10">Other Skills</div>
+
+      <div class="flex flex-wrap justify-center gap-5">
+        <Skill name="Gamemaker" image="./images/gamemaker.svg"/>
+        <Skill name="Unity" image="./images/unity-plain.svg"/>
+        <Skill name="Godot" image="./images/godot-original.svg"/>
+        <Skill name="Blender" image="./images/blender-original.svg"/>
+        <Skill name="Substance Painter" image="./images/substance-painter.svg"/>
+      </div>
+    </div>
 	</Window>
 </template>
